15bb80eaf94acf4b2c9a7b482e489cb3ee227b69
Mr. Yeti <aditya.ingle@outlook.com>
2020-12-28 13:22:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -27,30 +27,4 @@ client.on('message',(message)=>{
     }
 });
 
-client.login(TOKEN);*/
-
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-const { TOKEN, COMMAND_PREFIX } = process.env;
-
-const insults = require('./data/insults');
-
-client.once('ready', () => {
-  console.log('Luther Insult Bot is ready to insult you!');
-});
-
-client.on('message', (message) => {
-  if (message.author.bot || !message.content.startsWith(COMMAND_PREFIX)) return;
-
-  const args = message.content.split(' ');
-  const command = args[0].slice(COMMAND_PREFIX.length);
-
-  if (command.toLowerCase() === 'luther') {
-    message.reply(insults[Math.floor(Math.random() * insults.length)]);
-  }
-});
-
-// make sure this is the last of the file
 client.login(TOKEN);
